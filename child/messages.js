c8e07b87c23ca624ed8fb56c1164bac5a0402f1f
Tony G. Bolaño <tonybolanyo@gmail.com>
2020-07-28 12:44:53 +0200

    chore: strip last slash from referrer

--- child/messages.js
+++ child/messages.js
@@ -26,7 +26,7 @@ console.log('[CHILD] document.referrer', document.referrer);
 console.log('[CHILD] client ID', clientId);
 console.log('[CHILD] allowed origins', origin);
 
-if (origin !== document.referrer) {
+if (origin !== document.referrer.replace(/\/+$/, '')) {
   throw 'You are not allowed to embed this player';
 }
 
